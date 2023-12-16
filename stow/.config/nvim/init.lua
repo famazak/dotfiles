af3e6d01bd0d23125bcae0bbcd38f469e396cbcd
famazak <zakfama58@gmail.com>
2023-12-16 11:49:05 -0500

    chore: theme change
    change default theme style

--- stow/.config/nvim/init.lua
+++ stow/.config/nvim/init.lua
@@ -294,6 +294,7 @@ require('lazy').setup({
   {
     'sainnhe/gruvbox-material',
     config = function()
+      vim.g.gruvbox_material_background='hard'
       vim.cmd.colorscheme 'gruvbox-material'
       vim.g.gruvbox_material_better_performance=1
     end
