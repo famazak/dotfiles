1f7a314d0c72a77f65348b4a6a1409bdce940175
famazak <zakfama58@gmail.com>
2024-02-28 19:20:11 -0500

    chore: remove one more rust-tools thing

--- stow/.config/nvim/init.lua
+++ stow/.config/nvim/init.lua
@@ -452,17 +452,6 @@ require('telescope').setup {
   },
 }
 
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
-
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 -- Enable telescope harpoon extension
