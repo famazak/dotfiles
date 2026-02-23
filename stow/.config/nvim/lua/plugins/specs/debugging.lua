e833b991d8c0794fcf1bc24878c7f1ac7692c113
famazak <zakfama58@gmail.com>
2026-02-22 20:02:24 -0500

    configure typescript debugger

--- stow/.config/nvim/lua/plugins/specs/debugging.lua
+++ stow/.config/nvim/lua/plugins/specs/debugging.lua
@@ -40,6 +40,84 @@ return {
     end
   },
   {
+    'mxsdev/nvim-dap-vscode-js',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'microsoft/vscode-js-debug'
+    },
+    config = function()
+      require("dap-vscode-js").setup({
+        node_path = "node",
+        debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+      })
+
+      local dap = require('dap')
+      for _, language in ipairs({ 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' }) do
+        dap.configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require'dap.utils'.pick_process,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Jest Tests",
+            runtimeExecutable = "node",
+            runtimeArgs = {
+              "./node_modules/jest/bin/jest.js",
+              "--runInBand",
+            },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+          },
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Vitest Tests",
+            program = "${workspaceFolder}/node_modules/vitest/vitest.mjs",
+            args = { "--reporter=verbose", "--no-coverage" },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+          },
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Current Vitest File",
+            program = "${workspaceFolder}/node_modules/vitest/vitest.mjs",
+            args = { "run", "${relativeFile}", "--reporter=verbose", "--no-coverage" },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+          },
+        }
+      end
+    end
+  },
+  {
+    'microsoft/vscode-js-debug',
+    lazy = true,
+    build = function()
+      os.execute("npm install --legacy-peer-deps")
+      os.execute("npm run compile")
+    end
+  },
+  {
     'rcarriga/nvim-dap-ui',
     dependencies = {
       'mfussenegger/nvim-dap',
